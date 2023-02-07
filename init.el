9b8cebc38ab971ffa7ad7a3d99426d7cd7140f66
Miguel Calçado <37229666+mhcal@users.noreply.github.com>
2023-02-07 18:46:42 -0300

    Emacs config file

---
+++ init.el
@@ -0,0 +1,4 @@
+(tool-bar-mode -1)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(setq backward-delete-char-untabify-method 'hungry)
+(set-face-attribute 'region nil :background "#bfbfbf")
