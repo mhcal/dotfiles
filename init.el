751e38d9616d737e6cd5d719f93b08f00021ace6
Miguel Calçado <37229666+mhcal@users.noreply.github.com>
2024-08-30 23:31:03 -0300

    Delete init.el

--- init.el
+++
@@ -1,4 +0,0 @@
-(tool-bar-mode -1)
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-(setq backward-delete-char-untabify-method 'hungry)
-(set-face-attribute 'region nil :background "#bfbfbf")
